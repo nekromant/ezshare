a3f18cf0ddb0baf641a20e29c25f16c1161109c2
Andrew Andrianov <andrew@ncrmnt.org>
2022-07-28 15:11:46 +0300

    scripts: Update release script
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,8 +3,9 @@
 version=`toml get --toml-path pyproject.toml project.version`
 nextversion=$(echo ${version} | awk -F. -v OFS=. '{$NF += 1 ; print}')
 toml set --toml-path pyproject.toml project.version $nextversion
-git commit -s -m "v{nextversion} release"
-git push origin/master
+git add pyproject.toml
+git commit -s -m "v${nextversion} release"
+git push origin master
 python3 -m build
 twine check dist/*
 twine upload dist/*
