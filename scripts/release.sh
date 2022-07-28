9c491bb775b148f70647261c308ff0bf5e306bdd
Andrew Andrianov <andrew@ncrmnt.org>
2022-07-28 14:30:05 +0300

    Add packaging scripts
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

---
+++ scripts/release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+version=`toml get --toml-path pyproject.toml project.version`
+nextversion=$(echo ${version} | awk -F. -v OFS=. '{$NF += 1 ; print}')
+toml set --toml-path pyproject.toml project.version $nextversion
+git commit -s -m "v{nextversion} release"
+git push origin/master
+python3 -m build
+twine check dist/*
+twine upload dist/*
